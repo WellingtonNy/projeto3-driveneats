e70ac658700ae65c75b1c8917196d03bc7d97cf3
Wellington <Wellingtoncpfilho@gmail.com>
2023-01-09 16:14:56 -0300

    finalizado whatsApp

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function selecionarPrato(thisPrato) {
     document.querySelector('.prato .selecionado img').classList.add('mostrar')
     prato = document.querySelector('.prato .selecionado h3').innerText
     precoPrato = document.querySelector('.prato .selecionado .preco').innerText;
-    habilitarPedir ()
+    habilitarPedir()
 }
 
 
@@ -33,7 +33,7 @@ function selecionarBebida(thisBebida) {
     document.querySelector('.bebida .selecionado img').classList.add('mostrar')
     bebida = document.querySelector('.bebida .selecionado h3').innerText
     precoBebida = document.querySelector('.bebida .selecionado .preco').innerText;
-    habilitarPedir ()
+    habilitarPedir()
 }
 
 
@@ -47,36 +47,58 @@ function selecionarSobremesa(thisSobremesa) {
     document.querySelector('.sobremesa .selecionado img').classList.add('mostrar')
     sobremesa = document.querySelector('.sobremesa .selecionado h3').innerText
     precoSobremesa = document.querySelector('.sobremesa .selecionado .preco').innerText;
-    habilitarPedir ()
+    habilitarPedir()
 }
 
 
-function habilitarPedir (){
-if(prato !== undefined && bebida !== undefined && sobremesa !== undefined){
-    const BotaoPedir = document.querySelector('.bot .pedir')
-    BotaoPedir.classList.add('habilitado')
-    BotaoPedir.innerText = 'Fechar pedido'
-    BotaoPedir.removeAttribute('disabled')
-}
-}
-
-
-function conferir(){
-    document.querySelector('.modal').classList.remove('escondido')
-    document.querySelector('.modal .info .infoPrato p').innerText=`${prato}`
-    document.querySelector('.modal .info .infoPrato span').innerText=`${precoPrato}`
-    document.querySelector('.modal .info .infoBebida p').innerText=`${bebida}`
-    document.querySelector('.modal .info .infoBebida span').innerText=`${precoBebida}`
-    document.querySelector('.modal .info .infoSobremesa p').innerText=`${sobremesa}`
-    document.querySelector('.modal .info .infoSobremesa span').innerText=`${precoSobremesa}`
-    total()
-    document.querySelector('.modal .info .total span').innerText=`${precoTotal}`
-    nome=prompt('Digite seu Nome')
-    endereco=prompt('Digite seu Endereço')
+function habilitarPedir() {
+    if (prato !== undefined && bebida !== undefined && sobremesa !== undefined) {
+        const BotaoPedir = document.querySelector('.bot .pedir')
+        BotaoPedir.classList.add('habilitado')
+        BotaoPedir.innerText = 'Fechar pedido'
+        BotaoPedir.removeAttribute('disabled')
     }
+}
 
 
-function total(){
-    precoTotal=(Number(precoPrato.replace(',', '.')) +Number(precoBebida.replace(',', '.'))+ Number(precoSobremesa.replace(',', '.'))).toFixed(2).replace('.',',')
+function conferir() {
+    document.querySelector('.modal').classList.remove('escondido')
+    document.querySelector('.modal .info .infoPrato p').innerText = `${prato}`
+    document.querySelector('.modal .info .infoPrato span').innerText = `${precoPrato}`
+    document.querySelector('.modal .info .infoBebida p').innerText = `${bebida}`
+    document.querySelector('.modal .info .infoBebida span').innerText = `${precoBebida}`
+    document.querySelector('.modal .info .infoSobremesa p').innerText = `${sobremesa}`
+    document.querySelector('.modal .info .infoSobremesa span').innerText = `${precoSobremesa}`
+    total()
+    document.querySelector('.modal .info .total span').innerText = `${precoTotal}`
+    nome = prompt('Digite seu Nome')
+    endereco = prompt('Digite seu Endereço')
+}
+
+
+function total() {
+    precoTotal = (Number(precoPrato.replace(',', '.')) + Number(precoBebida.replace(',', '.')) + Number(precoSobremesa.replace(',', '.'))).toFixed(2).replace('.', ',')
     console.log(precoTotal)
 }
+
+
+function cancelarPedido() {
+    document.querySelector('.modal').classList.add('escondido')
+}
+
+
+function podePedir() {
+    const mensagem = 'https://wa.me/5521991750289?text=' + encodeURIComponent(whatsApp)
+    window.open(mensagem)
+}
+
+
+let whatsApp =
+    `Olá, gostaria de fazer o pedido:\n
+- Prato: ${prato} \n
+- Bebida: ${bebida} \n
+- Sobremesa: ${sobremesa} \n
+Total: R$ ${precoTotal} \n
+\n
+Nome: ${nome} \n
+Endereço: ${endereco}`
